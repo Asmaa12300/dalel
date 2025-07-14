cc36a2581f974d7d86a55695c7c51e415f0f4435
Lenovo <asmaaelneanaey1@gmail.com>
2025-07-14 14:11:11 +0300

    App Colors

---
+++ lib/core/utils/app_colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+abstract class AppColors{
+  static Color primarycolor =const Color(0xffC49E85);
+  static Color offwhite =const Color(0xffF8F4F9);
+  static Color deebBrown =const Color(0xff6B4B3E);
+  static Color deebGray =const Color(0xff6F6460);
+  static Color lightGray =const Color(0xffB4B4B4);
+  static Color black =const Color(0xff333333);
+
+
+
+
+}
